2f98530179545fb49e0c8ff0937ce6b502d21951
souk0712 <souk0712@gmail.com>
2022-05-02 22:26:08 +0900

    17204

--- src/week5/Java17204.java
+++ src/week5/Java17204.java
@@ -8,6 +8,27 @@ public class Java17204 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int j = 0;
+        int count = 0;
+        boolean[] check = new boolean[n];
+        while (j != k) {
+            count++;
+            j = arr[j];
+
+            if (check[j]) {
+                System.out.println(-1);
+                return;
+            }
+            check[j] = true;
+        }
+        System.out.println(count);
 
     }
 }
