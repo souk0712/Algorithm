4936646c6e101ed6006cdfb9d81edd267797ed67
이아름 <souk0712@naver.com>
2023-03-05 02:27:16 +0900

    주석 및 코드 정리

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -2,15 +2,15 @@
 class Solution {
     static int[] dx = {1, 0, 1, 0};    // 오른쪽위, 오른쪽, 오른쪽아래, 아래
     static int[] dy = {-1, 1, 1, 1};
-    
     static boolean winX = false;
     static boolean winO = false;
-    
+
     public int solution(String[] board) {
         char[][] map = new char[3][3];
         int countO = 0;
         int countX = 0;
 
+        // 개수 세기
         for (int i = 0; i < board.length; i++) {
             char[] c = board[i].toCharArray();
             for (int j = 0; j < c.length; j++) {
@@ -25,29 +25,33 @@ class Solution {
                 }
             }
         }
-        if (countO <countX)
+
+        // X가 더 많은 경우
+        if (countO < countX)
             return 0;
-        
+        // X와 O의 차가 1이상인 경우
         if (Math.abs(countO - countX) > 1)
             return 0;
 
         for (int i = 0; i < map.length; i++)
             for (int j = 0; j < map[i].length; j++)
-                for (int direct = 0; direct < dx.length; direct++)
-                    if (map[i][j] == 'X')
-                        checkFinish(map, direct, i, j, 1, 0);
-                    else if (map[i][j] == 'O')
-                        checkFinish(map, direct, i, j, 0, 1);
+                if (map[i][j] != '.')
+                    for (int direct = 0; direct < dx.length; direct++)
+                        if (map[i][j] == 'X')
+                            checkFinish(map, direct, i, j, 1, 0);
+                        else if (map[i][j] == 'O')
+                            checkFinish(map, direct, i, j, 0, 1);
 
+        // 둘 다 이기는 경우
         if (winX && winO) {
             return 0;
         } else {
+            // 둘 다 아직 이기기 전
             if (!winX && !winO) {
-                if(countO < countX)
-                    return 0;
-                else
-                    return 1;
-            } else if (winO && countO == countX ||
+                return 1;
+                // O가 이기는데 X 개수가 O와 같거나 더 많은 경우
+            } else if (winO && countO <= countX ||
+                    // X가 이기는데 O 개수가 더 많은 경우
                     winX && countO > countX)
                 return 0;
             else
@@ -56,7 +60,6 @@ class Solution {
     }
 
     private static void checkFinish(char[][] map, int direct, int i, int j, int x, int o) {
-
         if (o == 3) {
             winO = true;
             return;
