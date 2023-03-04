764bc3216e7dc806d189bbeb7fcd46c84fea5919
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-03-05 02:19:08 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 0.07 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -0,0 +1,76 @@
+
+class Solution {
+    static int[] dx = {-1, 0, 1, 1};    // 오른쪽위, 오른쪽, 오른쪽아래, 아래
+    static int[] dy = {1, 1, 1, 0};
+    
+    static boolean winX = false;
+    static boolean winO = false;
+    
+    public int solution(String[] board) {
+        char[][] map = new char[3][3];
+        int countO = 0;
+        int countX = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            char[] c = board[i].toCharArray();
+            for (int j = 0; j < c.length; j++) {
+                map[i][j] = c[j];
+                switch (c[j]) {
+                    case 'O' -> {
+                        countO++;
+                    }
+                    case 'X' -> {
+                        countX++;
+                    }
+                }
+            }
+        }
+        if (countO <countX)
+            return 0;
+        
+        if (Math.abs(countO - countX) > 1)
+            return 0;
+
+        for (int i = 0; i < map.length; i++)
+            for (int j = 0; j < map[i].length; j++)
+                for (int direct = 0; direct < dx.length; direct++)
+                    if (map[i][j] == 'X')
+                        checkFinish(map, direct, i, j, 1, 0);
+                    else if (map[i][j] == 'O')
+                        checkFinish(map, direct, i, j, 0, 1);
+
+        if (winX && winO) {
+            return 0;
+        } else {
+            if (!winX && !winO) {
+                if(countO < countX)
+                    return 0;
+                else
+                    return 1;
+            } else if (winO && countO == countX ||
+                    winX && countO > countX)
+                return 0;
+            else
+                return 1;
+        }
+    }
+
+    private static void checkFinish(char[][] map, int direct, int i, int j, int x, int o) {
+
+        if (o == 3) {
+            winO = true;
+            return;
+        }
+        if (x == 3) {
+            winX = true;
+            return;
+        }
+        int ii = i + dx[direct];
+        int jj = j + dy[direct];
+        if (ii >= 3 || jj >= 3 || ii < 0 || jj < 0) return;
+        if (map[ii][jj] == 'X')
+            checkFinish(map, direct, ii, jj, x + 1, o);
+        else if (map[ii][jj] == 'O')
+            checkFinish(map, direct, ii, jj, x, o + 1);
+    }
+}
