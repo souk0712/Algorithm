7789d6c4f2a322d7207f0a007381f3a3fbb52a94
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-05 19:50:56 +0900

    [unrated] Title: 코드 처리하기, Time: 0.53 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.kt
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.kt
@@ -1,18 +1,3 @@
 class Solution {
-    fun solution(code: String): String {
-        var answer =""
-        var mode = 0
-        code.forEachIndexed { idx, c ->
-            if(c == '1'){
-                if(mode==1) mode = 0 else mode =1
-            }else{
-                if(mode == 0 && idx % 2 == 0){
-                    answer += c
-                }else if(mode == 1 && idx %2 != 0){
-                    answer += c
-                }
-            }
-        }
-        return if(answer.isEmpty()) "EMPTY" else answer
-    }
+    fun solution(code: String): String = code.foldIndexed("" to 0) { idx, (ret, mode), c -> if(c == '1') ret to 1 - mode else if(mode == idx % 2) ret + c to mode else ret to mode }.first.ifEmpty {"EMPTY"}
 }
