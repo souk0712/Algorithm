4c947d588e66c009f0539ccf3204ce3b79acabd5
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-05 21:42:50 +0900

    [unrated] Title: 이어 붙인 수, Time: 21.67 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.kt
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.kt
@@ -1,5 +1,3 @@
 class Solution {
-    fun solution(num_list: IntArray): Int {
-        return num_list.filter{ it % 2 == 0 }.fold("") {acc, i -> acc + i}.toInt() + num_list.filter{ it % 2 == 1 }.fold(""){acc, i -> acc + i}.toInt()
-    }
+    fun solution(num_list: IntArray): Int = num_list.partition{it % 2 == 0}.toList().sumOf{ it.joinToString("").toInt() }
 }
