13a6c1b5cd542a74c07bb110ec2f21dffe02c5f9
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-06 17:57:00 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 14.11 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.kt
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.kt
@@ -1,10 +1,9 @@
 class Solution {
-    fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
-        queries.map { q ->
-            (q[0]..q[1]).filter { it % q[2] == 0 }.forEach {
+    fun solution(arr: IntArray, queries: Array<IntArray>): IntArray = arr.apply {
+        queries.forEach { (s, e, k) ->
+            (s..e).filter { it % k == 0 }.forEach {
                 arr[it] += 1
             }
         }
-        return arr
     }
 }
