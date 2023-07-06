b0a67fecac37acc83b9810e24c5bdae2ff9ff7d2
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-06 19:25:49 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 8.91 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.kt
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.kt
@@ -1,12 +1,5 @@
 class Solution {
-    fun solution(n: Int): IntArray {
-        var answer = intArrayOf(n)
-        var t = n
-        while(t > 1){
-            if(t % 2 == 0) t /= 2 
-            else t = 3 * t + 1
-            answer += t
-        }
-        return answer
+    fun solution(n: Int): List<Int> {
+        return generateSequence(n) { if (it == 1) null else if (it % 2 == 0) it / 2 else 3 * it + 1 }.toList()
     }
 }
