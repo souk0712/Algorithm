ce34230e0d34a21439259b4aba87ca535d17dfa6
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-06 17:34:39 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 20.64 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.kt
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.kt
@@ -1,20 +1,7 @@
-import kotlin.math.*
-
 class Solution {
     fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
-        var answer: IntArray = intArrayOf()
-
-        queries.forEach { (s, e, k) ->
-            var min = Int.MAX_VALUE
-            for (i in 0 .. arr.lastIndex) {
-                if (i in s..e && arr[i] > k) {
-                    min = min(arr[i], min)
-                }
-            }
-            answer += if (min == Int.MAX_VALUE)
-                -1
-            else min
-        }
-        return answer
+        return queries.map { q ->
+            (q[0]..q[1]).filter { arr[it] > q[2] }.minOfOrNull { arr[it] } ?: -1
+        }.toIntArray()
     }
 }
