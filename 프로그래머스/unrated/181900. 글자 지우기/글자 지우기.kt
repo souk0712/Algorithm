f2da220a172f63a6700ed3ae3987f729df3b834e
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-10 17:37:08 +0900

    [unrated] Title: 글자 지우기, Time: 13.90 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.kt
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.kt
@@ -1,12 +1,5 @@
 class Solution {
     fun solution(my_string: String, indices: IntArray): String {
-        val list = my_string.toMutableList()
-        indices.sortDescending()
-
-        for (index in indices){
-            list.removeAt(index)
-        }
-        
-        return list.joinToString("")
+        return my_string.filterIndexed { index, c -> index !in indices }
     }
 }
