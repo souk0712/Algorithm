ceea8b787ca59b2168e201b83bd28555f67c8d1a
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-10 15:42:45 +0900

    [unrated] Title: 세로 읽기, Time: 16.75 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.kt
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.kt
@@ -1,10 +1,5 @@
 class Solution {
     fun solution(my_string: String, m: Int, c: Int): String {
-        var answer: String = ""
-        for(i in my_string.indices step m){
-            val str = my_string.substring(i, i + m)
-            answer += str[c-1]
-        }
-        return answer
+        return my_string.chunked(m).map { it[c-1] }.joinToString("")
     }
 }
