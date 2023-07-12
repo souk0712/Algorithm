bf7c9d58a62e7ffe1eb578eb44d081188583ee1a
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-13 02:04:57 +0900

    [Silver III] Title: N과 M (3), Time: 376 ms, Memory: 112004 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static StringBuilder sb;
+    static int[] pick;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        pick = new int[M];
+
+        perm(0);
+
+        System.out.println(sb);
+    }
+
+    private static void perm(int cnt) {
+        if (cnt == M) {
+            for (int i : pick) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            pick[cnt] = i;
+            perm(cnt + 1);
+        }
+    }
+}
