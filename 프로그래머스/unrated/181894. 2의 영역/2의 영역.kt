0cccc3b467443ebe21b744fdecd13fc45ad4139f
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-14 03:04:16 +0900

    [unrated] Title: 2의 영역, Time: 15.61 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.kt
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.kt
@@ -1,23 +1,5 @@
 class Solution {
     fun solution(arr: IntArray): IntArray {
-        var first = -1
-        var last = -1
-
-        for (i in arr.indices) {
-            if (arr[i] == 2) {
-                if (first == -1)
-                    first = i
-                else
-                    last = i
-            }
-        }
-
-        return if (first == -1) {
-            intArrayOf(-1)
-        } else if (last == -1) {
-            intArrayOf(arr[first])
-        } else {
-            arr.sliceArray(first..last)
-        }
+        return if (arr.contains(2)) arr.copyOfRange(arr.indexOf(2), arr.lastIndexOf(2) + 1) else intArrayOf(-1)
     }
 }
