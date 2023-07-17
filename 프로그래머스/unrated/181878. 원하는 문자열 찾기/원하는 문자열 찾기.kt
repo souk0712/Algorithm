1ee7fce0ed8c8379775676356b6f115947b8fb1d
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-18 02:27:11 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 11.47 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.kt
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.kt
@@ -1,5 +1,5 @@
 class Solution {
     fun solution(myString: String, pat: String): Int {
-        return if(myString.toUpperCase().contains(pat.toUpperCase())) 1 else 0
+        return if (myString.uppercase().contains(pat.uppercase())) 1 else 0
     }
 }
