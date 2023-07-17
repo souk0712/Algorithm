3360968b500d3f6eee09929d00fb99dc403641bd
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-18 01:28:05 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 6.67 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.kt
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.kt
@@ -1,11 +1,7 @@
 class Solution {
     fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
         queries.forEach { ints: IntArray ->
-            for (i in arr.indices) {
-                if (i in (ints[0]..ints[1])) {
-                    arr[i]++
-                }
-            }
+            (ints[0]..ints[1]).forEach { arr[it] += 1 }
         }
         return arr
     }
