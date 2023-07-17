dd3cd1a1f2f53027340f2d9de2932a01faf6ea8f
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-18 01:36:33 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 20.31 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.kt
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.kt
@@ -1,13 +1,9 @@
 class Solution {
     fun solution(arr: IntArray): IntArray {
-        return arr.apply {
-            for (i in this.indices) {
-                if (this[i] >= 50 && this[i] % 2 == 0) {
-                    arr[i]/=2
-                } else if (this[i] < 50 && this[i] % 2 == 1) {
-                    arr[i]*=2
-                }
-            }
-        }
+        return arr.map {
+            if (it >= 50 && it % 2 == 0) it / 2
+            else if (it < 50 && it % 2 == 1) it * 2
+            else it
+        }.toIntArray()
     }
 }
