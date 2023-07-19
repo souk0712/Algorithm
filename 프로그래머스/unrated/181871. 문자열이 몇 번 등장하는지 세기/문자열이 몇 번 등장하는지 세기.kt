780b5724cbd34778baee77f98d7a6bee4e6796b7
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-19 17:47:07 +0900

    [unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 8.83 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.kt
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.kt
@@ -1,12 +1,3 @@
 class Solution {
-    fun solution(myString: String, pat: String): Int {
-        var answer = 0
-        for (i in 0..myString.length - pat.length) {
-            val chip = myString.substring(i, i + pat.length)
-            if (chip == pat) {
-                answer++
-            }
-        }
-        return answer
-    }
+    fun solution(myString: String, pat: String): Int = (0..myString.length - pat.length).map { myString.substring(it, it + pat.length) }.count { it == pat }
 }
