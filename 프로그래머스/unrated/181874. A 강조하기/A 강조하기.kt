2b4622ee8f5a67f523eb20a62407c1d23d19dae1
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-19 16:26:55 +0900

    [unrated] Title: A 강조하기, Time: 6.24 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.kt
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.kt
@@ -1,14 +1,5 @@
 class Solution {
     fun solution(myString: String): String {
-        var answer = ""
-        for (i in myString.indices) {
-            val c = myString[i]
-            answer += if (c == 'a' || c == 'A') {
-                c.uppercase()
-            } else {
-                c.lowercase()
-            }
-        }
-        return answer
+        return myString.lowercase().replace('a','A')
     }
 }
