b963f8328e66794981a271706217c0540c916c1d
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-20 18:01:53 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 29.52 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.kt
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.kt
@@ -1,12 +1,6 @@
 class Solution {
     fun solution(arr: IntArray, k: Int): IntArray {
-        val uniqueArr = arr.toSet().toList()
-        var result = uniqueArr.take(k).toIntArray()
-        if (result.size < k) {
-            repeat(k - result.size) {
-                result += -1
-            }
-        }
-        return result
+        val new = arr.distinct()
+        return (0 until k).map { if (it >= new.size) -1 else new[it] }.toIntArray()
     }
 }
