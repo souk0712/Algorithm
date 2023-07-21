461e417c08bf1be469915dfc8f8adb74f8f1ef5f
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-21 21:55:03 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 20.76 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.kt
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.kt
@@ -1,6 +1,5 @@
 class Solution {
     fun solution(arr: IntArray, n: Int): IntArray {
-        return (if (arr.size % 2 == 1) arr.mapIndexed { index, i -> if (index % 2 == 0) i + n else i }
-        else arr.mapIndexed { index, i -> if (index % 2 == 1) i + n else i }).toIntArray()
+        return arr.indices.map { arr[it] + if (it % 2 == if (arr.size % 2 == 0) 1 else 0) n else 0 }.toIntArray()
     }
 }
