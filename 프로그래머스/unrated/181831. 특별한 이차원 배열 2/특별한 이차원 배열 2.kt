5c16cb6ba112ee7d24bcdd5cada86b69bec2d42d
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-07-24 18:09:17 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.02 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(arr: Array<IntArray>): Int {
+        for(i in arr.indices){
+            for(j in arr[i].indices){
+                if(arr[i][j] != arr[j][i]){
+                    return 0
+                }
+            }
+        }
+        return 1
+    }
+}
