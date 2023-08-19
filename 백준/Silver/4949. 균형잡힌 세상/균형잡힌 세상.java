3db68febd856c95e03e8c94e498f36c4802c2bd8
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-08-20 03:20:36 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 168 ms, Memory: 16472 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -5,50 +5,47 @@ import java.util.Stack;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Stack<Character> stack;
-		StringBuilder sb = new StringBuilder();
-		String input = "";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        loop:
+        while (true) {
+            String input = br.readLine();
+            if (".".equals(input)) break;
+            else {
+                Stack<Character> stack = new Stack<>();
+                for (int i = 0; i < input.length(); i++) {
+                    char c = input.charAt(i);
+                    switch (c) {
+                        case '(':
+                        case '[':
+                            stack.push(c);
+                            break;
 
-		loop: while (true) {
-			stack = new Stack<>();
-			input = br.readLine();
-			if (input.equals(".")) {
-				break;
-			}
-			char[] temp = input.toCharArray();
-			for (int i = 0; i < temp.length; i++) {
-				switch (temp[i]) {
-				case '(':
-					stack.push(temp[i]);
-					break;
-				case '[':
-					stack.push(temp[i]);
-					break;
-				case ')':
-				case ']':
-					if (stack.isEmpty()) {
-						sb.append("no\n");
-						continue loop;
-					}
-					char p = stack.pop();
-					if (temp[i] == ')' && p == '(') {
-
-					} else if (temp[i] == ']' && p == '[') {
-
-					} else {
-						sb.append("no\n");
-						continue loop;
-					}
-					break;
-				}
-			}
-			if (stack.isEmpty())
-				sb.append("yes\n");
-			else
-				sb.append("no\n");
-		}
-		System.out.println(sb);
-	}
+                        case ')':
+                        case ']':
+                            if (!stack.isEmpty()) {
+                                char cc = stack.pop();
+                                if (c == ')' && cc != '(') {
+                                    sb.append("no").append("\n");
+                                    continue loop;
+                                } else if (c == ']' && cc != '[') {
+                                    sb.append("no").append("\n");
+                                    continue loop;
+                                }
+                            } else {
+                                sb.append("no").append("\n");
+                                continue loop;
+                            }
+                            break;
+                    }
+                }
+                if (stack.isEmpty())
+                    sb.append("yes").append("\n");
+                else
+                    sb.append("no").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
