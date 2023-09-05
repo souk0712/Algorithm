84606c57f70339db1500cc19dbc71f3412a6525d
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-09-05 23:31:12 +0900

    [unrated] Title: 추억 점수, Time: 0.04 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        Map<String, Integer> map = new HashMap<>();
+            
+        for(int i = 0; i < name.length; i++){
+            map.put(name[i], yearning[i]);
+        }
+        
+        for(int i = 0; i < photo.length; i++){
+            int score = 0;
+            for(int j = 0; j < photo[i].length; j++){
+                String str = photo[i][j];
+                if(map.containsKey(str)){
+                    score += map.get(str);
+                }
+            }
+            answer[i] = score;
+        }
+        
+        return answer;
+    }
+}
