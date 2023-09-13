0fd64dbe13e69be63fdbd164cffe9dc7d31e2bcf
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-09-14 04:36:19 +0900

    [level 1] Title: 소수 찾기, Time: 25.31 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,6 +1,24 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
+        boolean[] isNotPrime = new boolean[n + 1];
         int answer = 0;
+        
+        isNotPrime[0] = isNotPrime[1] = true;
+        for(int i = 2; i * i <= n; i++){
+            if(!isNotPrime[i]){
+                for(int j = i * i; j <= n; j += i){
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+        
+        for(int i = 1; i <= n; i++){
+            if(!isNotPrime[i]) answer++;
+        }
+        
         return answer;
     }
 }
