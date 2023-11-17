fcaeade545ad65b6e89c8144cc1c09d9d6ad064b
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-11-18 02:59:31 +0900

    [Gold IV] Title: 전력난, Time: 1008 ms, Memory: 249784 KB -BaekjoonHub

--- 백준/Gold/6497. 전력난/전력난.java
+++ 백준/Gold/6497. 전력난/전력난.java
@@ -1,71 +1,74 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, M;
-    static ArrayList<Edge>[] graph;
+    static int[] parent;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            PriorityQueue<Node> pq = new PriorityQueue<Node>();
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
-            if (M == 0 && N == 0) break;
-
-            int sum = 0;
-            graph = new ArrayList[M];
-            for (int i = 0; i < M; i++) graph[i] = new ArrayList<Edge>();
+            if (N == 0 && M == 0) break;
+            parent = new int[M];
+            for (int i = 0; i < M; i++) parent[i] = i;
+            int total = 0;
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
                 int z = Integer.parseInt(st.nextToken());
-                graph[x].add(new Edge(y, z));
-                graph[y].add(new Edge(x, z));
-                sum += z;
+                total += z;
+                pq.offer(new Node(x, y, z));
             }
-            sb.append(sum - prim(0, M)).append("\n");
+            sb.append(total - kruskal(pq)).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static int prim(int start, int M) {
-        boolean[] visit = new boolean[M];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(start, 0));
-        int total = 0;
+    private static int kruskal(PriorityQueue<Node> pq) {
+        int min = 0;
         while (!pq.isEmpty()) {
-            Edge edge = pq.poll();
-            int v = edge.w;
-            int cost = edge.cost;
-            if (visit[v]) continue;
-            visit[v] = true;
-            total += cost;
-            for (Edge e : graph[v]) {
-                if (!visit[e.w]) {
-                    pq.offer(e);
-                }
-            }
+            Node cur = pq.poll();
+            if (find(cur.v) == find(cur.w)) continue;
+            union(cur.v, cur.w);
+            min += cur.cost;
         }
-        return total;
+        return min;
     }
 
-    static class Edge implements Comparable<Edge> {
-        int w, cost;
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return;
+        if (a > b) parent[a] = b;
+        else parent[b] = a;
+    }
 
-        Edge(int w, int cost) {
+    public static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static class Node implements Comparable<Node> {
+        int v, w, cost;
+
+        Node(int v, int w, int cost) {
+            this.v = v;
             this.w = w;
             this.cost = cost;
         }
 
         @Override
-        public int compareTo(Edge o) {
+        public int compareTo(Node o) {
             return Integer.compare(cost, o.cost);
         }
     }
