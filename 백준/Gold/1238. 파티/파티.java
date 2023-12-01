6f5519833631d7eb5ccbe8c0074b896de4a9dcaa
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-12-01 20:45:52 +0900

    [Gold III] Title: 파티, Time: 588 ms, Memory: 56100 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -10,78 +10,65 @@ public class Main {
 
     static int N, M, X;
     static ArrayList<Node>[] list;
-    static int[][] dist;
+    static int[] dist, total;
+    static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-
+        dist = new int[N + 1];
+        total = new int[N + 1];
         list = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
-
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-
             list[a].add(new Node(b, c));
         }
 
-        dist = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++)
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-
         for (int i = 1; i <= N; i++) {
-            Dijkstra(i);
+            dijkstra(i);
+            total[i] = dist[X];
         }
+        dijkstra(X);
 
-        int[] sum = new int[N + 1];
         int max = Integer.MIN_VALUE;
-
         for (int i = 1; i <= N; i++) {
-            sum[i] = dist[X][i] + dist[i][X];
-            max = Math.max(sum[i], max);
+            max = Math.max(max, dist[i] + total[i]);
         }
-
         System.out.println(max);
     }
 
-    private static void Dijkstra(int x) {
-
-        // X -> 각 노드
-        boolean[] visited = new boolean[N + 1];
-
-        dist[x][x] = 0;
-
+    private static void dijkstra(int start) {
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        visit = new boolean[N + 1];
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(x, 0));
-
+        pq.offer(new Node(start, 0));
+        dist[start] = 0;
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
+            if (visit[cur.dest]) continue;
+            visit[cur.dest] = true;
 
-            if (visited[cur.w]) continue;
-            visited[cur.w] = true;
-
-            for (Node next : list[cur.w]) {
-                if (dist[x][next.w] > dist[x][cur.w] + next.cost) {
-                    dist[x][next.w] = dist[x][cur.w] + next.cost;
-
-                    pq.offer(new Node(next.w, dist[x][next.w]));
+            for (Node next : list[cur.dest]) {
+                if (dist[next.dest] > dist[cur.dest] + next.cost) {
+                    dist[next.dest] = dist[cur.dest] + next.cost;
+                    pq.offer(new Node(next.dest, dist[next.dest]));
                 }
             }
         }
     }
 
     static class Node implements Comparable<Node> {
-        int w, cost;
+        int dest, cost;
 
-        Node(int w, int cost) {
-            this.w = w;
+        Node(int dest, int cost) {
+            this.dest = dest;
             this.cost = cost;
         }
 
