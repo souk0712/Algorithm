26e76205b4c0c43258dd3f16a5be5ce35a24692e
Areum Lee <90661870+souk0712@users.noreply.github.com>
2023-12-26 14:48:42 +0900

    [Gold IV] Title: 도시 건설, Time: 1460 ms, Memory: 178032 KB -BaekjoonHub

--- 백준/Gold/21924. 도시 건설/도시 건설.java
+++ 백준/Gold/21924. 도시 건설/도시 건설.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -9,52 +8,44 @@ public class Main {
     static int N, M;
     static int[] parent;
 
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         parent = new int[N + 1];
         for (int i = 1; i <= N; i++) parent[i] = i;
-        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
         long total = 0;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            pq.offer(new Node(a, b, c));
-            total += c;
+            int to = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            pq.offer(new Node(to, from, cost));
+            total += cost;
         }
-        long ans = kruskal(pq);
-        if (isCheckAllPath()) {
-            System.out.println(total - ans);
-        } else {
-            System.out.println(-1);
-        }
-    }
 
-    private static boolean isCheckAllPath() {
-        int compare = find(1);
-        for (int i = 2; i <= N; i++) {
-            if (compare != find(i)) return false;
-        }
-        return true;
-    }
-
-    private static long kruskal(PriorityQueue<Node> pq) {
         long sum = 0;
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
-            if (find(cur.a) == find(cur.b)) continue;
-            union(cur.a, cur.b);
-            sum += cur.c;
+            if (find(cur.to) == find(cur.from)) continue;
+            union(cur.to, cur.from);
+            sum += cur.cost;
         }
-        return sum;
+        int p = find(1);
+        for (int i = 2; i <= N; i++) {
+            if (p != find(i)) {
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        System.out.println(total - sum);
     }
 
-    static void union(int a, int b) {
+    private static void union(int a, int b) {
         int aa = find(a);
         int bb = find(b);
         if (aa == bb) return;
@@ -67,18 +58,18 @@ public class Main {
         return parent[a] = find(parent[a]);
     }
 
-    public static class Node implements Comparable<Node> {
-        int a, b, c;
+    static class Node implements Comparable<Node> {
+        int to, from, cost;
 
-        Node(int a, int b, int c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
+        Node(int to, int from, int cost) {
+            this.to = to;
+            this.from = from;
+            this.cost = cost;
         }
 
         @Override
         public int compareTo(Node o) {
-            return Integer.compare(c, o.c);
+            return Integer.compare(cost, o.cost);
         }
     }
 }
