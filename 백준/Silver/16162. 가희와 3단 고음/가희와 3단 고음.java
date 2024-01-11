91d00a7a90ff46cc1016080cd276f70783ead83f
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-01-11 22:41:57 +0900

    [Silver IV] Title: 가희와 3단 고음, Time: 148 ms, Memory: 16992 KB -BaekjoonHub

---
+++ 백준/Silver/16162. 가희와 3단 고음/가희와 3단 고음.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int num = A;
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] == num) {
+                ans++;
+                num += D;
+            }
+        }
+        System.out.println(ans);
+    }
+}
