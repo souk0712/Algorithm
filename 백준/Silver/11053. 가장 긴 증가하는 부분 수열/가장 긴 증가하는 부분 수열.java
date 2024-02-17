d2ea13b438c5ffdd843afe9180bce3f5d50ea33f
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-02-18 01:19:11 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 144 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,62 +1,31 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int[] a, dp;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        a = new int[N];
-        dp = new int[N];
-        int LIS = 0;
-
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        ArrayList<Integer> dp = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-
-            int idx = BinarySearch(a[i], 0, LIS, LIS + 1);
-
-            if (idx == -1) {
-                dp[LIS++] = a[i];
+            int idx = Collections.binarySearch(dp, arr[i]);
+            if (idx < 0) {
+                idx = (idx + 1) * -1;
+            }
+            if (idx >= dp.size()) {
+                dp.add(arr[i]);
             } else {
-                dp[idx] = a[i];
+                dp.set(idx, arr[i]);
             }
         }
-        System.out.println(LIS);
-    }
-
-    private static int BinarySearch(int target, int start, int end, int size) {
-        int res = 0;
-
-        while (start <= end) {
-            // 중앙값
-            int mid = (start + end) / 2;
-
-            // target이 dp의 중앙값보다 작거나 같은 경우
-            if (target <= dp[mid]) {
-                res = mid;
-                end = mid - 1;
-            }
-            // target이 dp의 중앙값보다 큰 경우
-            else {
-                start = mid + 1;
-            }
-        }
-
-        // dp에서 삽입될 위치를 찾지 못한 경우(모든 수보다 큰 경우)
-        if (start == size) {
-            return -1;
-        }
-        // dp에서 삽입될 위치를 찾은 경우
-        else {
-            return res;
-        }
+        System.out.println(dp.size());
     }
 }
