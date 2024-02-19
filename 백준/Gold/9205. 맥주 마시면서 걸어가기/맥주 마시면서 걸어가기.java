2d57db0ae402114274d294610495dc89bfa67e65
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-02-20 01:09:42 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 152 ms, Memory: 14764 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,75 +1,68 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] start, dest;
-    static int[][] comb;
-    static boolean[] visit;
-    static boolean check;
 
-    public static void main(String[] args) throws IOException {
+    static int N;
+    static int startX, startY;
+    static int[][] arr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
-            int n = Integer.parseInt(br.readLine());
-            start = new int[2];
-            dest = new int[2];
-            comb = new int[n][2];
-            check = false;
-            visit = new boolean[n];
-            st = new StringTokenizer(br.readLine());
-            start[0] = Integer.parseInt(st.nextToken());
-            start[1] = Integer.parseInt(st.nextToken());
-            for (int i = 0; i < n; i++) {
+            N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            startX = Integer.parseInt(st.nextToken());
+            startY = Integer.parseInt(st.nextToken());
+            arr = new int[N + 1][2];
+            for (int i = 0; i < N + 1; i++) {
                 st = new StringTokenizer(br.readLine());
-                comb[i][0] = Integer.parseInt(st.nextToken());
-                comb[i][1] = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                arr[i][0] = x;
+                arr[i][1] = y;
             }
-            st = new StringTokenizer(br.readLine());
-            dest[0] = Integer.parseInt(st.nextToken());
-            dest[1] = Integer.parseInt(st.nextToken());
-            dfs(start[0], start[1]);
-            if (check) {
-                sb.append("happy").append("\n");
-            } else {
-                sb.append("sad").append("\n");
-            }
+            bfs();
         }
         System.out.println(sb);
     }
 
-    private static void dfs(int x, int y) {
-        if (x == dest[0] && y == dest[1]) {
-            check = true;
-            return;
-        }
-
-        for (int i = 0; i < comb.length; i++) {
-            int[] nextComb = comb[i];
-            int diff = getDiff(x, y, nextComb[0], nextComb[1]);
-            if (!visit[i] && isEnoughBeer(diff)) {
-                visit[i] = true;
-                dfs(nextComb[0], nextComb[1]);
+    private static void bfs() {
+        Deque<int[]> dq = new ArrayDeque<>();
+        boolean[] visit = new boolean[N + 1];
+        for (int i = 0; i <= N; i++) {
+            int dist = getDist(startX, startY, arr[i][0], arr[i][1]);
+            if (dist <= 1000) {
+                dq.offer(new int[]{i, arr[i][0], arr[i][1]});
             }
         }
-        int d = getDiff(x, y, dest[0], dest[1]);
-        if (isEnoughBeer(d)) {
-            dfs(dest[0], dest[1]);
+
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
+            if (cur[0] == N) {
+                sb.append("happy").append("\n");
+                return;
+            }
+
+            for (int k = 0; k < N + 1; k++) {
+                if (visit[k]) continue;
+                int dist = getDist(cur[1], cur[2], arr[k][0], arr[k][1]);
+                if (dist <= 1000) {
+                    visit[k] = true;
+                    dq.offer(new int[]{k, arr[k][0], arr[k][1]});
+                }
+            }
         }
+        sb.append("sad").append("\n");
     }
 
-    private static boolean isEnoughBeer(int diff) {
-        int n = diff / 50;
-        if (n == 20) {
-            return diff % 50 == 0;
-        } else return n < 20;
-    }
-
-    private static int getDiff(int x1, int y1, int x2, int y2) {
+    private static int getDist(int x1, int y1, int x2, int y2) {
         return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
 }
