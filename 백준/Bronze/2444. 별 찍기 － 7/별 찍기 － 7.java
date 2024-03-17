c6f6b1994378d603a1e677328ab83bfb6b35f798
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-03-17 23:43:21 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 84 ms, Memory: 11728 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = i; j < N; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            for (int j = 1; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = i; j < N; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            for (int j = 1; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
