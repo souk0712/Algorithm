8eb94985f97e5842488d259454dc4d218024837b
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-03-18 17:40:27 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int div = 10_007;
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] % div + dp[i - 2] % div) % div;
+        }
+        System.out.println(dp[n]);
+    }
+}
