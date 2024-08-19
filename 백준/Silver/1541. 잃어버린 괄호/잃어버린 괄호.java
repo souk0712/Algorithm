17c5ca18dd1e31f5bc78ae3aff713f7135367016
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-08-19 23:23:31 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 72 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -3,59 +3,35 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        ArrayList<Integer> intList = new ArrayList<>();
-        ArrayList<Character> charList = new ArrayList<>();
-        StringBuilder temp = new StringBuilder();
-
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (c == '-' || c == '+') {
-                intList.add(Integer.parseInt(temp.toString()));
-                temp = new StringBuilder();
-                charList.add(c);
-            } else {
-                temp.append(c);
+        String s = br.readLine();
+        int sum = 0;
+        String[] arr = s.split("[+-]");
+        int[] num = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            num[i] = Integer.parseInt(arr[i]);
+        }
+        ArrayList<Character> oper = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+                oper.add(s.charAt(i));
             }
         }
-        intList.add(Integer.parseInt(temp.toString()));
-
-        int sum = 0;
-        boolean check = false;
-        if (intList.size() == charList.size()) {
-            for (int i = 0; i < intList.size(); i++) {
-                int ii = intList.get(i);
-                char c = charList.get(i);
-
-                if (c == '-') {
-                    check = true;
-                }
-
-                if (check) {
-                    sum -= ii;
-                } else {
-                    sum += ii;
-                }
+        boolean flag = true;
+        int i = 0, j = 0;
+        if (num.length != oper.size()) {
+            sum += num[0];
+            i = 1;
+        }
+        for (; i < num.length; i++, j++) {
+            if (oper.get(j) == '-') {
+                flag = false;
             }
-        } else {
-            sum = intList.get(0);
-
-            for (int i = 1; i < intList.size(); i++) {
-                int ii = intList.get(i);
-                char c = charList.get(i - 1);
-
-                if (c == '-') {
-                    check = true;
-                }
-
-                if (check) {
-                    sum -= ii;
-                } else {
-                    sum += ii;
-                }
+            if (flag) {
+                sum += num[i];
+            } else {
+                sum -= num[i];
             }
         }
 
