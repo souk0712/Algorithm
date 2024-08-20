3dbcb4c4b8111761b4fa1aef1819cfa494829f72
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-08-21 00:47:10 +0900

    [Silver III] Title: 퇴사, Time: 64 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -4,32 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int max = Integer.MIN_VALUE;
-        int[][] map = new int[N][2];
+        int[] T = new int[N];
+        int[] P = new int[N];
         int[] dp = new int[N + 1];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            map[i][0] = Integer.parseInt(st.nextToken());
-            map[i][1] = Integer.parseInt(st.nextToken());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
         }
-
-        for (int i = 0; i < N; i++) {
-
-            // 일을 했을 경우
-            if (i + map[i][0] <= N) {
-                dp[i + map[i][0]] = Math.max(dp[i + map[i][0]], dp[i] + map[i][1]);
+        for (int i = N - 1; i >= 0; i--) {
+            int next = i + T[i];
+            if (next <= N) {
+                dp[i] = Math.max(dp[i + 1], dp[next] + P[i]);
+            }else{
+                dp[i] = Math.max(dp[i + 1], dp[i]);
             }
-
-            // 일을 안했을 경우
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
-            max = Math.max(max, dp[i + 1]);
         }
-
-        System.out.println(max);
+        System.out.println(dp[0]);
     }
 }
