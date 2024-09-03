34ada3977a1c5cc6807ffcc7fb647fc874fb7792
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-09-03 22:17:38 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 69.20 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -4,87 +4,60 @@ import java.util.*;
 class Solution {
     
     static int[][] map;
-    static PriorityQueue<Integer> pq;
     
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
         map = new int[rows][columns];
-        pq = new PriorityQueue<>();
-        initMap(map);
-        
-        for(int i = 0; i < queries.length; i++){
-            pq = new PriorityQueue<>();
-            int sx = queries[i][0] - 1;
-            int sy = queries[i][1] - 1;
-            int ex = queries[i][2] - 1;
-            int ey = queries[i][3] - 1;
-            int res = map[sx][sy];
-            pq.offer(res);
-            
-            res = rotateArray(res, sx, sy, ex, ey, 1);
-            res = rotateArray(res, sx, sy, ex, ey, 2);
-            res = rotateArray(res, sx, sy, ex, ey, 3);
-            res = rotateArray(res, sx, sy, ex, ey, 4);
-            
-            answer[i] = pq.poll();
-        }
-
-        return answer;
-    }
-    
-    public static void initMap(int[][] map){
-        int index = 1;
-        for(int i = 0; i < map.length; i++){
-            for(int j = 0; j < map[i].length; j++){
+        for(int i = 0, index = 1; i < rows; i++){
+            for(int j = 0; j < columns; j++){
                 map[i][j] = index++;
             }
         }
+        
+        for(int i = 0; i < queries.length; i++){
+            int[] query = queries[i];
+            answer[i] = rotate(rows, columns, query);
+        }
+        
+        return answer;
     }
     
-    public static int rotateArray(int res, int sx, int sy, int ex, int ey, int d){
-        int temp = res;
-        switch(d){
-                // 오른쪽 가로
-                case 1:
-                
-                    for(int j = sy + 1; j <= ey; j++){
-                        int t = map[sx][j];
-                        pq.offer(t);
-                        map[sx][j] = temp;
-                        temp = t;
-                    }
-                break;
-                
-                // 오른쪽 세로
-                case 2:
-                    for(int j = sx + 1; j <= ex; j++){   
-                        int t = map[j][ey];
-                        pq.offer(t);
-                        map[j][ey] = temp;
-                        temp = t;
-                    }
-                break;
-                
-                // 왼쪽 가로
-                case 3:
-                    for(int j = ey - 1; j >= sy; j--){
-                        int t = map[ex][j];
-                        pq.offer(t);
-                        map[ex][j] = temp;
-                        temp = t;
-                    }
-                break;
-                
-                // 왼쪽 세로
-                default:
-                    for(int j = ex - 1; j >= sx; j--){   
-                        int t = map[j][sy];
-                        pq.offer(t);
-                        map[j][sy] = temp;
-                        temp = t;
-                    }
-                break;
+    static int rotate(int rows, int columns, int[] query){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int x1 = query[0] - 1;
+        int y1 = query[1] - 1;
+        int x2 = query[2] - 1;
+        int y2 = query[3] - 1;
+        int value = map[x1 + 1][y1];
+        // top: x1
+        for(int i = y1; i <= y2; i++){
+            int temp = map[x1][i];
+            pq.offer(temp);
+            map[x1][i] = value;
+            value = temp;
         }
-        return temp;
+        // right: y2
+        for(int i = x1 + 1; i <= x2; i++){
+            int temp = map[i][y2];
+            pq.offer(temp);
+            map[i][y2] = value;
+            value = temp;
+        }
+        // bottom: x2
+        for(int i = y2 - 1; i >= y1; i--){
+            int temp = map[x2][i];
+            pq.offer(temp);
+            map[x2][i] = value;
+            value = temp;
+        }
+        // left: y1
+        for(int i = x2 - 1; i >= x1; i--){
+            int temp = map[i][y1];
+            pq.offer(temp);
+            map[i][y1] = value;
+            value = temp;
+        }
+        
+        return pq.poll();
     }
 }
