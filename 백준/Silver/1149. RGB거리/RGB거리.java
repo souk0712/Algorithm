495e591cfb2721e4451903bd2365d653716b34d8
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-09-05 00:37:27 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 12100 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,31 +3,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] map = new int[N][3];
         int[][] dp = new int[N][3];
+        int ans = Integer.MAX_VALUE;
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
                 map[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = 3001;
             }
         }
-        for (int j = 0; j < 3; j++) {
-            dp[0][j] = map[0][j];
-        }
-
-        for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + map[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + map[i][1];
-            dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + map[i][2];
-        }
-
-        int ans = Integer.MAX_VALUE;
         for (int i = 0; i < 3; i++) {
-            ans = Math.min(dp[N - 1][i], ans);
+            dp[0][i] = map[0][i];
+        }
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (k == j) continue;
+                    dp[i][j] = Math.min(map[i][j] + dp[i - 1][k], dp[i][j]);
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            ans = Math.min(ans, dp[N - 1][i]);
         }
         System.out.println(ans);
     }
