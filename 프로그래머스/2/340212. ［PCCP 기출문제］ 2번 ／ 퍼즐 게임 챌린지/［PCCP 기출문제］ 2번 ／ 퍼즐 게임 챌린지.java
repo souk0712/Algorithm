1b9161438598d7f72df151145e215910a09d29b2
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-11-12 22:24:56 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 31.97 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -7,9 +7,11 @@ class Solution {
         int N = diffs.length;
         int left = 1;
         int right = 300_000;
+        
         while(left <= right){
             int level = (left + right) / 2;
             long time = solvePuzzles(diffs, times, level);
+            
             if(time <= limit){
                 answer = level;
                 right = level - 1;
@@ -23,16 +25,19 @@ class Solution {
     
     static long solvePuzzles(int[] diffs, int[] times, int level){
         long res = times[0];
+        
         for(int i = 1; i < diffs.length; i++){
             int diff = diffs[i];
             int cur = times[i];
             int prev = times[i - 1];
+            
             if(diff <= level){
                 res += cur;
             }else{
                 res += (diff - level) * (cur + prev) + cur;
             }
         }
+        
         return res;
     }
 }
