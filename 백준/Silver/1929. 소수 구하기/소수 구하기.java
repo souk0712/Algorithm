f53f801bb7781d2df360ccffe9c26abab676422d
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-11-21 00:57:07 +0900

    [Silver III] Title: 소수 구하기, Time: 160 ms, Memory: 18844 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -3,28 +3,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static boolean[] isNotPrime;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
+        isNotPrime = new boolean[N + 1];
+        isNotPrime[0] = isNotPrime[1] = true;
 
+        isPrimeNumber(N);
         StringBuilder sb = new StringBuilder();
         for (int i = M; i <= N; i++) {
-            if (isPrimeNumber(i)) {
+            if (!isNotPrime[i]) {
                 sb.append(i).append("\n");
             }
         }
         System.out.println(sb);
     }
 
-    private static boolean isPrimeNumber(int num) {
-        if (num == 1) return false;
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                return false;
+    private static void isPrimeNumber(int maxNum) {
+        for (int i = 2; i * i <= maxNum; i++) {
+            for (int j = 2; j * i <= maxNum; j++) {
+                isNotPrime[i * j] = true;
             }
         }
-        return true;
     }
 }
