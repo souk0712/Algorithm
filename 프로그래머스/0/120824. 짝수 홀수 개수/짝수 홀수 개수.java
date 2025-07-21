aa0985f4d922b72f9a7e7bd54495e63c5aecc553
Areum Lee <90661870+souk0712@users.noreply.github.com>
2025-07-21 23:16:58 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                a++;
+            }else{
+                b++;
+            }
+        }
+        return new int[]{a, b};
+    }
+}
