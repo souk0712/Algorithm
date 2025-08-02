590ce25766b82849dfcd1dede5a891438e793149
Areum Lee <90661870+souk0712@users.noreply.github.com>
2025-08-02 22:41:27 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.05 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.kt
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.kt
@@ -1,16 +1,8 @@
 class Solution {
     fun solution(sides: IntArray): Int {
-        var answer: Int = 0
-        // case 1. 둘 중 한 변이 가장 긴 변인 경우
-        val max = Math.max(sides[0], sides[1])
-        val diff = Math.abs(sides[0] - sides[1])
-        for(i in diff + 1 .. max){
-            answer++
-        }
-        // case 2. 나머지 한 변이 가장 긴 변인 경우
-        for(i in max + 1 until sides[0] + sides[1]){
-            answer++
-        }
-        return answer
+        var high = sides[0] + sides[1]
+        var low = Math.abs(sides[0] - sides[1])
+
+        return high - low - 1
     }
 }
