1b6c29dc4613f352aa88e8345a6aa1993b539f0d
Areum Lee <90661870+souk0712@users.noreply.github.com>
2025-08-03 23:16:59 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 18.02 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.kt
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.kt
@@ -1,5 +1,6 @@
 class Solution {
     fun solution(my_string: String): Int {
-        return my_string.replace(Regex("[^0-9]"), " ").split(" ").filter {it != ""}.map{it.toInt()}.sum()
+        return my_string.split("[^0-9]".toRegex())
+            .sumOf{ it.toIntOrNull() ?: 0 }
     }
 }
