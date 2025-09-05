0b716561e69e4aedf41fdc975aef211f9c097f59
Areum Lee <90661870+souk0712@users.noreply.github.com>
2025-09-05 22:49:21 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.12 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
@@ -1,5 +1,5 @@
 class Solution {
     fun solution(my_string: String, letter: String): String {
-        return my_string.filter { it != letter.toCharArray()[0]}.toString()
+        return my_string.filterNot { it == letter[0]}
     }
 }
