170fea78c3af1ead88d2dc7d05072abb9e282c44
Areum Lee <90661870+souk0712@users.noreply.github.com>
2025-10-14 14:46:01 +0900

    Title: 숫자 차례로 출력하기, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

--- 251014/숫자 차례로 출력하기/to-output-numerically.java
+++ 251014/숫자 차례로 출력하기/to-output-numerically.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= N; i++){
             sb.append(i).append(" ");
         }
