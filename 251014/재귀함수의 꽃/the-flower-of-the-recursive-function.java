86a64ab756284d123431721c051df62e3121956d
Areum Lee <90661870+souk0712@users.noreply.github.com>
2025-10-14 14:56:47 +0900

    Title: 재귀함수의 꽃, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

--- 251014/재귀함수의 꽃/the-flower-of-the-recursive-function.java
+++ 251014/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        loop(5);
+        loop(n);
     }
 
     static void loop(int num){
