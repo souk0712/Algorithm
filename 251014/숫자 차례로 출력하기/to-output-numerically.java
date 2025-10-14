9552c93c42731322e5e428c97532ec3d4b89146e
Areum Lee <90661870+souk0712@users.noreply.github.com>
2025-10-14 14:45:20 +0900

    Title: 숫자 차례로 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251014/숫자 차례로 출력하기/to-output-numerically.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= N; i++){
+            sb.append(i).append(" ");
+        }
+        sb.append("\n");
+        for(int i = N; i >= 1; i--){
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
